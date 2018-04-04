686b383f27dc3062a4c008c4ac5ffc56f20bfd14
Theodoros Giannakopoulos <tyiannak@gmail.com>
2018-04-05 02:22:37 +0300

    clean

--- menu_items.js
+++
@@ -1,5 +0,0 @@
-var MENU_ITEMS = [ ['HOME', "index.html", null], 
-  		   ['About', "About.html", null], 
-                   ['Publications', "Publications.html", null], 
-		   ['Software', "Software.html", null], 
-		   ['Contact @', "Contact.html", null], ]; 
